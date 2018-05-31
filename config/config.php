d12957b62b111d12c463eba751c30077f661d6db
vitorfraga <vitor.augusto.fraga@gmail.com>
2018-05-31 18:24:19 -0300

    reestruturação do código

---
+++ config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'postgres';
+$config['db']['pass']   = '123456';
+$config['db']['dbname'] = 'papaer';
+$config['db']['port'] = '5433';
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+require 'vendor/autoload.php';
+require 'config/constants.php';
+require 'config/config.php';
+
+$app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+
+$container['view'] = new \Slim\Views\PhpRenderer('resources/views/');
+
+$app->get('/', function ($request, $response) {
+
+    $response = $this->view->render($response, 'template.phtml');
+
+    return $response;
+});
+
+
+// Exemplo de rota que recebe as variáveis
+$app->get('/{nome}', function ($request, $response) {
+
+	$nome = $request->getAttribute('nome');
+    $response = $this->view->render($response, 'template.phtml', $vars);
+
+    return $response;
+});
+
+
+$app->run();
+
